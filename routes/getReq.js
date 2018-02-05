dc5c18ddcfc02f9c4c896b5619a5f365f8aa3a08
Anmol varma <anmol_varma@github.com>
2018-02-05 13:59:40 +0530

    problem in nesting of promises

--- routes/getReq.js
+++ routes/getReq.js
@@ -2,6 +2,7 @@ const api = require('../getDataFromApi');
 const http = require('https');
 const rp = require('request-promise');
 
+
 module.exports = [
   {
     method: 'GET',
@@ -11,27 +12,45 @@ module.exports = [
       const apiDataValue = (value) => {
         const waitToFetchData = Promise.resolve(JSON.parse(value));
         const rateList = [];
-        waitToFetchData.then((list) => {
-          list.books.forEach((book) => {
-            const url = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${book.id}`;
-            return rp(url).then((data) => {
-              const entry = book;
-              entry.rating = JSON.parse(data).rating;
-              rateList.push(entry);
+        waitToFetchData.then(list => list.books.forEach((book) => {
+          console.log('promise1');
+          // const url = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${book.id}`;
+          // return rp(url).then((data) => {
+          //   const entry = book;
+          //   entry.rating = JSON.parse(data).rating;
+          //   rateList.push(entry);
+          // });
+
+          // console.log('data callback');
+          // const entry = book;
+          // entry.rating = JSON.parse(chunk).rating;
+          // return rateList.push(entry);
+
+          const url = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${book.id}`;
+          const fetchRatingForeach = new Promise((resol) => {
+            console.log('promise2');
+            const httpPromise = new Promise(() => {
+              http.get(url, (data) => {
+                console.log('in http get');
+                data.setEncoding('utf8');
+                const prom = new Promise((resolve) => {
+                  data.on('data', (chunk) => {
+                    console.log('data callback');
+                    const entry = book;
+                    entry.rating = JSON.parse(chunk).rating;
+                    rateList.push(entry);
+                    resolve(chunk);
+                  });
+                });
+                return prom;
+              });
             });
-            // const url = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${book.id}`;
-            // http.get(url, (data) => {
-            //   data.setEncoding('utf8');
-            //   data.on('data', (chunk) => {
-            //     const entry = book;
-            //     entry.rating = JSON.parse(chunk).rating;
-            //     rateList.push(entry);
-            //   });
-            // });
-            // console.log('in list.books.array');
+            return httpPromise;
           });
-        }).then(() => {
-          console.log(rateList, 'result');
+          return fetchRatingForeach;
+          // return fetchRatingForeach;
+        })).then(() => {
+          console.log(rateList);
           response(rateList);
         });
       };
